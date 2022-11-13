65b1f7ed763ab9ea5c8700a0e4d0766b3e5f4e7f
rasedprogrammer <rasedprogrammer@gmail.com>
2022-11-13 18:16:27 +0600

    adding sidebar styleing

--- src/components/OwnerActivity/OwnerActivity.js
+++ src/components/OwnerActivity/OwnerActivity.js
@@ -3,12 +3,13 @@ import "./OwnerActivity.css";
 
 const OwnerActivity = (props) => {
 	const { gymCart } = props;
-	const [secondTimes, setSecondTimes] = useState([]);
+	const [secondTimes, setSecondTimes] = useState([0]);
 
 	let totalSeconds = 0;
 	for (const cart of gymCart) {
 		totalSeconds = totalSeconds + cart.second;
 	}
+	// let setSecondTimes = 0;
 
 	const brackTimes = (event) => {
 		const time = event.target.innerText;
@@ -29,18 +30,32 @@ const OwnerActivity = (props) => {
 				<div className="personal-info"></div>
 			</div>
 			<div className="break-container">
-				<h3>Add A Brack</h3>
-				<div id="bracks-times" className="bracks-times">
-					<button onClick={brackTimes}>10s</button>
-					<button onClick={brackTimes}>20s</button>
-					<button onClick={brackTimes}>30s</button>
-					<button onClick={brackTimes}>40s</button>
-					<button onClick={brackTimes}>50s</button>
+				<h2>Add A Brack</h2>
+				<div className="bracks-times">
+					<button className="timeBtn" onClick={brackTimes}>
+						10s
+					</button>
+					<button className="timeBtn" onClick={brackTimes}>
+						20s
+					</button>
+					<button className="timeBtn" onClick={brackTimes}>
+						30s
+					</button>
+					<button className="timeBtn" onClick={brackTimes}>
+						40s
+					</button>
+					<button className="timeBtn" onClick={brackTimes}>
+						50s
+					</button>
 				</div>
 			</div>
-			<h3>Exercise Details</h3>
-			<h5>Exercise Time {totalSeconds} seconds</h5>
-			<h5>time: {secondTimes}</h5>
+			<div className="showBrackAndTimes">
+				<h2>Exercise Details</h2>
+				<h4>
+					<b>Exercise Time</b> {totalSeconds} seconds
+				</h4>
+				<h4>Brack time: {secondTimes}</h4>
+			</div>
 		</div>
 	);
 };
